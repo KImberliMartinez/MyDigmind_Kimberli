b2f79539e65c4b34f9628f85250e6477b22f0af5
KImberliMartinez <159958568+KImberliMartinez@users.noreply.github.com>
2025-09-28 22:46:03 -0700

    parte 1

---
+++ Digimind/app/src/main/java/martinez/kimberli/digimind/Recordatorio.kt
@@ -0,0 +1,7 @@
+package martinez.kimberli.digimind
+
+import java.io.Serializable
+
+class Recordatorio (var dias:String,
+                    var tiempo:String,
+                    var nombre:String): Serializable
